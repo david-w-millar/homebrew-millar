902247b41b3a8f3ef5f1997371308baffacfb1cc
David W Millar <david.w.millar@gmail.com>
2016-08-25 05:32:40 -0400

    added javacpp formula

---
+++ javacpp.rb
@@ -0,0 +1,18 @@
+class Javacpp < Formula
+  desc "The missing bridge between Java and native C++"
+  homepage "https://github.com/bytedeco/javacpp"
+  url "http://search.maven.org/remotecontent?filepath=org/bytedeco/javacpp/1.2.3/javacpp-1.2.3-bin.zip"
+  # version "1.2.3"
+  sha256 "b57e8add9d2ae67cf93e87b4964e54969daa810d0abb224bc80c7d38e8f4f6a6"
+
+  depends_on :java
+
+  def install
+    libexec.install "javacpp.jar"
+    bin.write_jar_script libexec/"javacpp.jar", "javacpp", "$JAVA_OPTS"
+  end
+
+  test do
+    system "true"
+  end
+end
